a50f1d18e88e9ee14a69912e665a8c376abe552b
Tasticoco <114155288+Tasticoco@users.noreply.github.com>
2024-05-03 13:43:32 +0200

    Init

---
+++ src/main/java/gre/lab2/groupX/Main.java
@@ -0,0 +1,14 @@
+package gre.lab2.groupX;
+
+import java.io.IOException;
+
+public final class Main {
+  public static void main(String[] args) throws IOException {
+    // TODO
+    //  - Renommage du package ;
+    //  - Écrire le code dans le package de votre groupe et UNIQUEMENT celui-ci ;
+    //  - Documentation soignée comprenant :
+    //    - la javadoc, avec auteurs et description des implémentations ;
+    //    - des commentaires sur les différentes parties de vos algorithmes.
+  }
+}
